3529187f3fa79a4a4731e0fd5010a691d017f9ed
VT <vtewari26@gmail.com>
2020-05-30 13:37:35 +0530

    add fish config file

---
+++ config.fish
@@ -0,0 +1,46 @@
+set -gx PATH $HOME/bin $PATH
+set -gx PATH $HOME/.deno/bin $PATH
+set -gx PATH $HOME/.nvm/versions/node/v10.19.0/bin $PATH
+set -gx EDITOR nvim
+
+# GO Setup
+set -gx PATH $HOME/tools/lang/go/current/bin $PATH
+set -gx GOPATH $HOME/go
+set -gx PATH $GOPATH/bin $PATH
+
+alias vim nvim
+
+# Fish git prompt
+set __fish_git_prompt_showuntrackedfiles 'yes'
+set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showstashstate ''
+set __fish_git_prompt_showupstream 'none'
+set -g fish_prompt_pwd_dir_length 3
+
+# colored man output
+# from http://linuxtidbits.wordpress.com/2009/03/23/less-colors-for-man-pages/
+setenv LESS_TERMCAP_mb \e'[01;31m'       # begin blinking
+setenv LESS_TERMCAP_md \e'[01;38;5;74m'  # begin bold
+setenv LESS_TERMCAP_me \e'[0m'           # end mode
+setenv LESS_TERMCAP_se \e'[0m'           # end standout-mode
+setenv LESS_TERMCAP_so \e'[38;5;246m'    # begin standout-mode - info box
+setenv LESS_TERMCAP_ue \e'[0m'           # end underline
+setenv LESS_TERMCAP_us \e'[04;38;5;146m' # begin underline
+
+function fish_prompt
+	set_color brblack
+	echo -n "["(date "+%H:%M")"] "
+	set_color blue
+	echo -n (hostname)
+	if [ $PWD != $HOME ]
+		set_color brblack
+		echo -n ':'
+		set_color yellow
+		echo -n (basename $PWD)
+	end
+	set_color green
+	printf '%s ' (__fish_git_prompt)
+	set_color red
+	echo -n '| '
+	set_color normal
+end
